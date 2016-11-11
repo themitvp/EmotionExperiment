4c88a9ead6d3ebd065856fc332dcf4fa6777df1b
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2016-11-11 21:01:24 +0100

    server updates

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ var url = 'mongodb://localhost:27017/db';
 mongoose.connect(url);
 
 app.use(express.static('./public')); 
-app.use(bodyParser.json({limit: '50mb', type: 'application/vnd.api+json'})); // parse application/json
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', 'extended': 'true'}));
 
 
 // routes ======================================================================
